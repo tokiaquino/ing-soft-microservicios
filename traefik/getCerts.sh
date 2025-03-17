e7785d3ffb7e9d33bb0e01a1e52df38b184f2bec
Roberto Aquino <toki.aquino@gmail.com>
2025-03-17 16:28:19 -0300

    Agregando traefik al proyecto

---
+++ traefik/getCerts.sh
@@ -0,0 +1,3 @@
+# Para la instalacion de mkcert
+# https://github.com/FiloSottile/mkcert?tab=readme-ov-file
+mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "ingesoftcurso.localhost" "*.ingesoftcurso.localhost" "ingesoftcurso.local" "*.ingesoftcurso.local"bas

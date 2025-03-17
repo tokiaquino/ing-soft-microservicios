509b1d72e3ee0bac560fc31f3ced4c9ab5365eb7
Roberto Aquino <toki.aquino@gmail.com>
2025-03-17 16:26:53 -0300

    Agregando traefik a ms-beta

--- ms-beta/app.py
+++ ms-beta/app.py
@@ -13,7 +13,7 @@ def healthcheck():
 @cache.cached(timeout=600)
 def show_compras():
     url_compras = os.getenv('URL_MS_ALPHA')
-    compras = requests.get(f'{url_compras}/compras').json()
+    compras = requests.get(f'{url_compras}/compras', verify=False).json()
     return compras, 200
 
 if __name__ == '__main__':
